b9a3ff10966f4fdeaebbbd42a5b5c08edd77ba17
Simon Wanner <simon+git@skyrising.xyz>
2022-10-12 10:44:03 +0200

    Fix crash on pipe timeout

--- modules/whitelist/commands/pipe.js
+++ modules/whitelist/commands/pipe.js
@@ -2,7 +2,7 @@ import fs from 'fs/promises'
 
 export async function sendCommands(pipe, ...commands) {
     const controller = new AbortController()
-    const timeout = setTimeout(controller.abort, 5000)
+    const timeout = setTimeout(controller.abort.bind(controller), 5000)
     await fs.writeFile(pipe, commands.map(cmd => cmd + '\n').join(''), {flag: 'a', signal: controller.signal})
     clearTimeout(timeout)
 }
